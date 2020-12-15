21dec01a049d690a5ed1ede444e92e184187d60c
abdullah jamshed <abdullahjamshad01@gmail.com>
2020-12-15 19:46:09 +0500

    updates

--- src/store/reducers/homeReducer.js
+++ src/store/reducers/homeReducer.js
@@ -27,6 +27,11 @@ export default (state = INITIAL_STATE, action) => {
         ...state,
         selectedCategory: action.payload.category,
       };
+    case 'SELECTED_CATEGOR4':
+      return {
+        ...state,
+        selectedCategory: action.payload.category,
+      };
     default:
       return state;
   }
